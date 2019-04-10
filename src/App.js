1f7d58555f471bfa13d210976cdf3b088b26f988
Developirv <icastellanosalvarez@gmail.com>
2019-04-09 22:45:59 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+class App extends Component {
+  render() {
+    return (
+      
+        <div className="container">
+          <h2>Welcome to the Eat My Shorts: A Simpsons Podcast companion app!</h2>
+        
+        </div>
+
+    );
+  }
+}
+
+
+export default App;
