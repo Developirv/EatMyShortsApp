ec21e751d3c51acd066a1cc7ea33c540520df14e
Developirv <icastellanosalvarez@gmail.com>
2019-04-11 16:41:37 -0500

    added procfile server js file and heroku dependencies to package json

---
+++ server.js
@@ -0,0 +1,20 @@
+onst express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+// Configure both serve-favicon & static middlewares
+// to serve from the production 'build' folder
+app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
+app.use(express.static(path.join(__dirname, 'build')));
+
+
+// The following "catch all" route (note the *)is necessary
+// for a SPA's client-side routing to properly work
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  });
